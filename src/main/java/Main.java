73f85b051a383280d714ac0b315e3292103dc1d0
Luciano <lucianoneimark@gmail.com>
2024-07-15 12:37:06 -0300

    user-agent endpoint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,26 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    private static String headerValue(String headerName, List<String> headers) {
+        for (String header: headers) {
+            String[] parts = header.split(":");
+            String name = parts[0].toLowerCase();
+            String value = parts[1];
+            if (name.equals(headerName)) {
+                return value.trim();
+            }
+        }
+        return "";
+    }
+
+    private static String responseBuilder(String statusLine, String responseHeaders, String responseBody) {
+        return statusLine + "\r\n" + responseHeaders + "\r\n\r\n" + responseBody;
+    }
+
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
@@ -31,26 +49,41 @@ public class Main {
             String requestLinePath = requestLine.split(" ")[1];
             String[] pathParts = requestLinePath.split("/"); // /abc/hello/world -> ['', 'abc', 'hello', 'world', '']
 
+            List<String> headers = new ArrayList<>();
+            String line;
+            while (!(line = reader.readLine()).isEmpty()) {
+                headers.add(line);
+            }
+
             String root = pathParts.length == 0 ? "" : pathParts[1];
 
             switch (root) {
                 case (""):
-                    clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String responseRoot = responseBuilder("HTTP/1.1 200 OK", "", "");
+                    clientSocket.getOutputStream().write(responseRoot.getBytes());
                     break;
                 case ("echo"):
-                    clientSocket.getOutputStream().write(
-                            ("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
-                                    pathParts[2].length() + "\r\n\r\n" + pathParts[2]).getBytes());
+                    String responseEcho = responseBuilder("HTTP/1.1 200 OK","Content-Type: text/plain\n" +
+                                    "Content-Length: "  + pathParts[2].length(), pathParts[2]);
+                    clientSocket.getOutputStream().write(responseEcho.getBytes());
                     break;
 
+                case ("user-agent"):
+                    String userAgent = headerValue("user-agent", headers);
+                    String responseUA = responseBuilder("HTTP/1.1 200 OK", "Content-Type: text/plain\n" +
+                            "Content-Length: " + userAgent.length(), userAgent);
+                    clientSocket.getOutputStream().write(responseUA.getBytes());
+                    break;
+                default:
+                    clientSocket.getOutputStream().write(responseBuilder("HTTP/1.1 404 Not Found", "", "").getBytes());
             }
 
-
-            clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
 
 
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
     }
+
+
 }
